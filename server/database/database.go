4410dc3936818bd8502b513c37bffa1450a8b786
Denis Papathanasiou <denis@papathanasiou.org>
2016-07-16 18:51:54 -0400

    Added a generic database access library in Go, based on the 'pq' driver

---
+++ server/database/database.go
@@ -0,0 +1,65 @@
+// Copyright Banrai LLC. All rights reserved. Use of this source code is
+// governed by the license that can be found in the LICENSE file.
+
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type DBConnection struct {
+	DBName  string
+	User    string
+	Pass    string
+	SSLMode bool
+}
+
+// Connect to the database with the given coordinates, and invoke the
+// function, which gets passed a map of all the prepared statements
+func WithDatabase(dbCoords DBConnection, fn func(map[string]*sql.Stmt)) {
+	preparedStatements := []string{ACCOUNT_INSERT,
+		ACCOUNT_UPDATE,
+		ACCOUNT_DELETE,
+		ACCOUNT_LOOKUP_BY_ID,
+		ACCOUNT_LOOKUP_BY_EMAIL,
+		SESSION_INSERT,
+		SESSION_UPDATE,
+		SESSION_CLEANUP,
+		SESSION_LOOKUP_BY_CODE,
+		SESSION_LOOKUP_BY_ACCOUNT,
+		PK_INSERT,
+		PK_UPDATE,
+		PK_DELETE,
+		PK_LOOKUP}
+
+	sslMode := "disable"
+	if dbCoords.SSLMode {
+		sslMode = "enable"
+	}
+
+	db, dbErr := sql.Open("postgres",
+		fmt.Sprintf("user=%s dbname=%s password=%s sslmode=%s",
+			dbCoords.User,
+			dbCoords.DBName,
+			dbCoords.Pass,
+			sslMode))
+	if dbErr != nil {
+		log.Fatal(dbErr)
+	}
+	defer db.Close()
+
+	statements := map[string]*sql.Stmt{}
+	for _, p := range preparedStatements {
+		stmt, err := db.Prepare(p)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			statements[p] = stmt
+		}
+	}
+
+	fn(statements)
+}
