7f9b3fac5d6c80feeb5408524c5f31319d28fa78
Denis Papathanasiou <denis@papathanasiou.org>
2016-12-19 20:15:27 -0500

    In addition to uploading a pk from a file, added a form option to use a url pointing to an armored text resource

--- html/js/upload.js
+++ html/js/upload.js
@@ -1,3 +1,16 @@
+TEAMWORK.focusUpload = function () {
+    $("#publicKeyUrl").val();
+    $("#key-url").hide(300);
+    $("#key-upload").show(300);
+}
+
+TEAMWORK.focusUrl = function () {
+    $("#selectedFile").val();
+    $("#key-upload").hide(300);
+    $("#key-url").show(300);
+    $("#publicKeyUrl").focus();
+}
+
 $(function(){
     if( TEAMWORK.session === null && TEAMWORK.person === null ) {
 	$("#userEmail").focus();
@@ -7,5 +20,19 @@ $(function(){
             label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
 	$("#selectedFile").html('<i class="fa fa-file-text-o"></i> '+label);
 	$("#selectedFile").css('padding-top', '0.5em');
-    });      
+    });
+    $("#keyTypeUpload").on('change', function() {
+	if( $(this).is(':checked') ) {
+	    TEAMWORK.focusUpload();
+	} else {
+	    TEAMWORK.focusUrl();
+	}
+    });
+    $("#keyTypeURL").on('change', function() {
+	if( $(this).is(':checked') ) {
+	    TEAMWORK.focusUrl();
+	} else {
+	    TEAMWORK.focusUpload();
+	}
+    });
 });
